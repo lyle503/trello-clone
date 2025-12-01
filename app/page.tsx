3407e0d694c7ac41bdf499f383fc5391e7ed399e
lyle503 <lylef84@googlemail.com>
2025-12-01 17:51:29 +0000

    Centering main page text

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,20 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-linear-to-br from-blue-50 via-white to-purple-50">
       <Navbar />
-      <main>
-        <div className="flex flex-col items-center pt-50">
-          <h1 className="text-3xl sm:text-5xl my-5">Welcome to TrelloClone</h1>
-          <p className="text-sm text-center mx-4 sm:text-lg">
-            A website built with{" "}
-            <a href="https://nextjs.org/" target="_blank">
-              Next.js
-            </a>{" "}
-            and{" "}
-            <a href="https://supabase.com/" target="_blank">
-              Supabase
-            </a>
-            , using TypeScript and TailwindCSS
-          </p>
-          <p className="text-xs my-5">built by Lyle Frankgate</p>
-        </div>
+      <main className="min-h-[90vh] flex flex-col items-center justify-center text-center">
+        <h1 className="text-3xl sm:text-5xl my-5">Welcome to TrelloClone</h1>
+        <p className="text-sm text-center mx-4 sm:text-lg">
+          A website built with{" "}
+          <a href="https://nextjs.org/" target="_blank">
+            Next.js
+          </a>{" "}
+          and{" "}
+          <a href="https://supabase.com/" target="_blank">
+            Supabase
+          </a>
+          , using TypeScript and TailwindCSS
+        </p>
+        <p className="text-xs my-5">built by Lyle Frankgate</p>
         <div className="flex flex-col justify-center items-center my-10">
           <GuestButton />
         </div>
